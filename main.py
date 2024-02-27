4a1d57a23b6ccc24d4b985eedb05c51db1b70946
dinixz <148535002+dinixz@users.noreply.github.com>
2024-02-27 11:11:01 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 
-
 def generateBoard():
     board = []
     for i in range(6):
@@ -27,8 +26,8 @@ def inputPlayer(letter):
         return ['b', 'w']
 
 def askForFirstPlayer():
-    # perguntar ao jogador quem é que deve jogar primeiro, as opções
-    #   podem ser por exemplo "white", "black" ou "random" e retorna o valor relacionado
+    #perguntar ao jogador quem é que deve jogar primeiro, as opções
+    #podem ser por exemplo "white", "black" ou "random" e retorna o valor relacionado
     valid = False
     while not valid:
         piece = input("Queres ser w, b or r? ")
@@ -38,7 +37,6 @@ def askForFirstPlayer():
             m = random.randint(0, 1)
             return "w" if m == 0 else "b"
 
-
 def testMoveValidity(board, col):
     # testa se a jogada é válida se a casa esta vazia e qual a posição esta vazia
     # retorna a posição que esta vazia ou false
@@ -47,27 +45,22 @@ def testMoveValidity(board, col):
             return 5 - i
     return False
 
-#print(testMoveValidity([['-', '-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-']], 1))
-
 def testMove(board, col):
+
     if col > 6 or col < 0:
         print("Fora das margens")
         return False
 
     position = testMoveValidity(board, col)
 
-    if not position:
+    if not isinstance(position, int):
         print("Coluna não disponível")
         return False
+
     return position
 
-#print(testMove([['w', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-']], 0))
-
-def move(board, turn, col):
-    position = testMove(board, col)
-    if isinstance(position, int):
-        board[position][col] = turn
-
+def move(board, turn, col, line):
+    board[line][col] = turn
 
 def askForNextMove(board, turn):
     while True:
@@ -81,16 +74,34 @@ def askForNextMove(board, turn):
         if isinstance(move, bool):
             print("A jogada não é válida. Tente novamente.")
         else:
-            return (turn, move_col)
+            return (turn, move_col, move)
 
+def winner(board):
 
+    for row in range(6):
+        for col in range(7):
 
+            #horizontal
+            if col <= 3:
+                if (board[row][col] == board[row][col + 1] == board[row][col + 2] == board[row][col + 3] and board[row][col] != '-'):
+                    return board[row][col]
 
-# print(askForNextMove([['-', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-'], ['w', '-', '-', '-', '-', '-', '-']], 'w'))
+            #vertical
+            if row <= 2:
+                if (board[row][col] == board[row + 1][col] == board[row + 2][col] == board[row + 3][col] and board[row][col] != '-'):
+                    return board[row][col]
 
-# # def winner(board):
-# #     #retorna se algum jogador fez 4 em linha caso nao retorna falso
-# #     raise (NotImplementedError)
+            #diagonal
+            if (col <= 3 and row <= 2):
+                if (board[row][col] == board[row + 1][col + 1] == board[row + 2][col + 2] == board[row + 3][col + 3] and board[row][col] != '-'):
+                    return board[row][col]
+
+            #diagonal
+            if (col <= 3 and row >= 3):
+                if (board[row][col] == board[row - 1][col + 1] == board[row - 2][col + 2] == board[row - 3][col + 3] and board[row][col] != '-'):
+                    return board[row][col]
+
+    return False
 
 def boardCopy(board):
     # se faz uma copia do tabuleiro no estado atual
@@ -102,7 +113,6 @@ def boardCopy(board):
         boardCopy.append(colCopy)
     return boardCopy
 
-
 def availableMoveAi(board):
     # retorna uma lista com movimentos possiveis para a IA
     acc = []
@@ -111,7 +121,6 @@ def availableMoveAi(board):
         if isinstance(position, int):
             acc.append((i, position))
     return acc
-
 
 # def AIMove():
 #     # executa o movimento da IA, supostamente aqui que implementaremos a IA
@@ -128,18 +137,28 @@ def main():
 
     valid = True
     while valid or len(availableMoveAi) > 0:
+
         printBoard(board)
+
+        win = winner(board)
+        if not isinstance(win, bool):
+            print('Winner is ' + win)
+            return win
+
         print("First Player")
-        moviment = askForNextMove(board, start_p)
-        move(board, moviment[0], moviment[1])
+        turn, col, line = askForNextMove(board, start_p)
+        move(board, turn, col, line)
 
         printBoard(board)
+
+        win = winner(board)
+        if not isinstance(win, bool):
+            print('Winner is ' + win)
+            return win
+
         print("Second Player")
-        move_col2 = askForNextMove(board, sec_p)
-        move(board, move_col2[0], move_col2[1])
-
-        valid = True
-
+        turn, col, line = askForNextMove(board, sec_p)
+        move(board, turn, col, line)
 
 if __name__ == '__main__':
     main()
