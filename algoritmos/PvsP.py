c96f4703548e34f0b2dc769df58bfdb5fcaf2c49
dinixz <148535002+dinixz@users.noreply.github.com>
2024-02-28 18:08:15 +0000

    Add files via upload

--- algoritmos/PvsP.py
+++ algoritmos/PvsP.py
@@ -4,8 +4,8 @@ from game.connectFour import *
 def gamePvsP(board,start_p, sec_p):
     valid = True
 
-    while valid or len(availableMoveAi) > 0:
-        printBoard(board)
+    while valid or len(availableMove) > 0:
+        print(board)
         win = winner(board)
         if not isinstance(win, bool):
             print('Winner is ' + win)
@@ -15,13 +15,16 @@ def gamePvsP(board,start_p, sec_p):
         turn, col, line = askForNextMove(board, start_p)
         move(board, turn, col, line)
 
-
+        print(board)
         win = winner(board)
         if not isinstance(win, bool):
+
+            if win == 'Tie': 
+                print(win)
+
             print('Winner is ' + win)
             return win
         
-        printBoard(board)
         print("Second Player")
         turn, col, line = askForNextMove(board, sec_p)
         move(board, turn, col, line)
